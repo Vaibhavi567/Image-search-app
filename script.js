73205265b1724618492ffe42b67c2167833a2e6e
Vaibhavi Shah <vshah20032002@gmail.com>
2024-01-04 09:46:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const accessKey = "XKJhJwfYWO2DNOa5sjTcahQDJB9dirhytq5bpPrTtdY";
+
+// connecting the html elements with js or we can say importing the attribute from html to js
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+// searching images vala ko input kar hain and page numbers that will get increment if user wants to see more images
+let inputData = "";
+let page = 1;
+
+//input data will store all the keywords which user is typing
+//main part function declaration
+
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  page++;
+
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click", () => {
+  searchImages();
+});
